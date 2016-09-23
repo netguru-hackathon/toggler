0020339305d3181cb063650a85c1772a52f1998f
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2016-09-23 11:43:35 +0200

    Refactor to allow default project names

--- toggl_manager.rb
+++ toggl_manager.rb
@@ -3,18 +3,17 @@ module Toggler
 
     def initialize
       @default_billable = ENV["TOGGLER_BILLABLE"] || false
-      @api = Toggler::API::Connect.new.api
+      @api = TogglV8::API.new(ENV["TOGGLER_API_KEY"])
       @user = api.me(true)
       @workspaces = api.my_workspaces(user)
-      # @default_project = { name: "hackaton", id: "342" }
     end
 
-    def start_entry(workspace = default_workspace["name"],
+    def start_entry(description = nil,
+                    project_name = default_project["name"],
                     billable = default_billable,
-                    description = nil)
-                    # project = default_project["name"],
-      wid = workspace_id(workspace)
-      pid = 22925160
+                    workspace_name = default_workspace["name"])
+      wid = workspace_id(workspace_name)
+      pid = project_id(workspace_name, project_name)
       new_entry_attributes = {
         "wid" => wid,
         "pid" => pid,
@@ -25,16 +24,18 @@ module Toggler
       }
       new_entry_attributes["description"] = description if description
       api.create_time_entry(new_entry_attributes)
-      "time entry started" if api.create_time_entry(new_entry_attributes)
+      return "time entry started" if api.create_time_entry(new_entry_attributes)
+      "error adding time entry"
     end
 
     def stop_entry
       time_entry_id = api.get_current_time_entry&.[]("id")
       return "no running time entry" unless time_entry_id
-      "time entry stopped" if api.stop_time_entry(time_entry_id)
+      return "time entry stopped" if api.stop_time_entry(time_entry_id)
+      "error stopping time entry"
     end
 
-    def list_projects(workspace_name: default_workspace["name"])
+    def list_projects(workspace_name = default_workspace["name"])
       wid = workspace_id(workspace_name)
       api.projects(wid)
     end
@@ -47,14 +48,17 @@ module Toggler
       @default_wid ||= workspaces.first
     end
 
+    def default_project
+      list_projects(default_workspace["name"]).first
+    end
+
     def workspace_id(workspace_name)
       workspaces.find { |w| w["name"] == workspace_name }["id"]
     end
 
     def project_id(workspace_name, project_name)
-      list_projects(workspace_name: workspace_name)
+      list_projects(workspace_name)
         .find { |p| p["name"] == project_name }["id"]
     end
-
   end
 end
